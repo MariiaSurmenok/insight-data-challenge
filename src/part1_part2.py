3676d70855faf9c7910320333503383bf4481c8f
Mariia <mariasurmenok@gmail.com>
2017-04-01 15:54:29 -0700

    added 2 features: count most active hosts and most popular resources

--- src/part1_part2.py
+++ src/part1_part2.py
@@ -1,19 +1,41 @@
 import re
+from collections import Counter
 
-str1 = 'onyx.southwind.net - - [01/Jul/1995:00:03:03 -0400] "GET /images/KSC-94EC-412-small.gif HTTP/1.0" 200 20484 '
-str2 = 'ix-war-mi1-20.ix.netcom.com - - [01/Jul/1995:00:05:13 -0400] "GET /shuttle/missions/sts-78/news HTTP/1.0" 302 - '
+def parse_log_line(log_line):
+    """
+    Parse log string into ip, date, request_method, url, protocol, response_code, and response_length
+    Return result as a dictionary
+    """
+    pattern = '(?P<ip>\S+).*' + \
+              '\[(?P<date>.+)\]\s*' + \
+              '"(?P<request_method>\S+)\s+(?P<url>\S+)\s+(?P<protocol>\S+)"\s*' + \
+              '(?P<response_code>\d+)\s+(?P<response_length>\d+|-)'
 
-pattern = '(?P<ip>\S+).*' + \
-          '\[(?P<date>.+)\]\s*' + \
-          '"(?P<request_method>\S+)\s+(?P<url>\S+)\s+(?P<protocol>\S+)"\s*' + \
-          '(?P<response_code>\d+)\s+(?P<response_length>\d+|-)'
-
-regex = re.compile(pattern)
-result = regex.match(str1)
+    regex = re.compile(pattern)
+    result = regex.match(log_line)
+    if result:
+        return result.groupdict()
 
 
-print(result.group("ip"))
-print(result.group("date"))
-print(result.group("url"))
-print(result.group("response_code"))
-print(result.group("response_length"))
+# count frequency of ip
+# count bandwidth of resources
+ip_frequency = Counter()
+resources = Counter()
+# filename = "../insight_testsuite/tests/test_features/log_input/log.txt"
+filename = "../log_input/log.txt"
+try:
+    with open(filename) as f_obj:
+        for line in f_obj:
+            data = parse_log_line(line)
+
+            if data:
+                # Calculate hosts frequency (feature 1)
+                ip_frequency[data["ip"]] += 1
+                # Calculate bandwidth resources
+                if data["response_length"].strip() != "-":
+                    resources[data["url"]] += int(data["response_length"])
+except FileNotFoundError:
+    print("count_host_frequency: File not found")
+
+print(ip_frequency.most_common(10))
+print(resources.most_common(10))
